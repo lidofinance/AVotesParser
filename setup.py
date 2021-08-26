0269aaceea7efe0f98966d5282d26e0058103ebe
Dmitri Ivakhnenko <dmit.ivh@gmail.com>
2021-08-26 19:24:06 +0300

    Refactoring: add dependencies

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     install_requires=[
         'requests~=2.26.0',
         'pysha3~=1.0.0',
-        'eth-brownie~=1.16.0'
+        'eth-brownie~=1.16.0',
+        'web3~=5.23.0'
     ]
 )

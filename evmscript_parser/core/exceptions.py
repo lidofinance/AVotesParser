b45180ca38cb1c56c4c4d7609892dd2fa8b2898b
Dmitri Ivakhnenko <dmit.ivh@gmail.com>
2021-08-26 19:20:21 +0300

    Refactoring: add specific exceptions for API calling

--- evmscript_parser/core/exceptions.py
+++ evmscript_parser/core/exceptions.py
@@ -1,10 +1,15 @@
+"""
+Exceptions.
+"""
+
+
 # ============================================================================
 # =========================== Parsing stage exceptions =======================
 # ============================================================================
 
 class ParseStructureError(TypeError):
     """
-    The base type of exception for exceptions at parsing stage.
+    The base type for exceptions at parsing stage.
     """
 
     pass
@@ -20,3 +25,37 @@ class ParseMismatchLength(ParseStructureError):
         message = f'Length of {field_name} should be: {expected}; ' \
                   f'received: {received}.'
         super().__init__(message)
+
+
+# ============================================================================
+# =========================== Decoding stage exceptions ======================
+# ============================================================================
+
+
+class ABIGettingError(IOError):
+    """
+    The base type for exceptions ot ABI getting.
+    """
+
+    pass
+
+
+class ABIEtherscanNetworkError(ABIGettingError):
+    """
+    Exception while calling Etherscan API.
+    """
+
+    pass
+
+
+class ABIEtherscanStatusCode(ABIGettingError):
+    """
+    Etherscan API return failure status code.
+    """
+
+    def __init__(self, status_code: str, message: str, result: str):
+        """Get error info and forward formatted message to super"""
+        msg = f'Code: {status_code}. ' \
+              f'Message: {message}. ' \
+              f'Result: {result}.'
+        super().__init__(msg)

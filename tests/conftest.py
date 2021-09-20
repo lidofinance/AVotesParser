ec338f7491d53a3d84cb26f9d61b32c82fc04db1
Dmitri Ivakhnenko <dmit.ivh@gmail.com>
2021-09-20 15:28:09 +0300

    Tests: add infura project id as argument

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
 """Settings for tests."""
+import os
+
+import brownie.network
 import pytest
 
 
@@ -8,9 +11,33 @@ def pytest_addoption(parser):
         '--apikey', type=str,
         default=None, help='API key for Etherscan.'
     )
+    parser.addoption(
+        '--infura-id', type=str,
+        default=None, help='Infura project ID '
+                           'for interaction with goerli.'
+    )
 
 
 @pytest.fixture(scope='session')
 def api_key(pytestconfig) -> str:
     """Return apikey from CLI parameters."""
     return pytestconfig.getoption('apikey')
+
+
+@pytest.fixture(scope='session')
+def infura_prt_id(pytestconfig) -> str:
+    """Return WEB3_INFURA_PROJECT_ID."""
+    return pytestconfig.getoption('infura_id')
+
+
+@pytest.fixture(scope='session')
+def target_net() -> str:
+    """Get target net."""
+    return 'goerli'
+
+
+@pytest.fixture(scope='session', autouse=True)
+def prepare_brownie_network(infura_prt_id):
+    """Set INFURA environment variable and disconnect from dev net."""
+    os.environ['WEB3_INFURA_PROJECT_ID'] = infura_prt_id
+    brownie.network.disconnect()

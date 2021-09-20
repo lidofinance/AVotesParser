0c6d3200b1aa988d190c4616c9044ced6517e3ab
Dmitri Ivakhnenko <dmit.ivh@gmail.com>
2021-09-20 19:14:50 +0300

    Update documentation

--- evmscript_parser/core/ABI/provider.py
+++ evmscript_parser/core/ABI/provider.py
@@ -171,14 +171,25 @@ class ABIProviderCombined(
 def get_cached_etherscan_api(
         api_key: str, net: str
 ) -> CachedStorage[ABIKey, ABI]:
-    """Return prepared instance of CachedStorage with API provider."""
+    """
+    Return prepared instance of CachedStorage with API provider.
+
+    :param api_key: str, Etherscan API token.
+    :param net: str, the name of target network.
+    :return: CachedStorage[ABIKey, ABI]
+    """
     return CachedStorage(ABIProviderEtherscanAPI(api_key, net))
 
 
 def get_cached_local_interfaces(
         interfaces_directory: str
 ) -> CachedStorage[ABIKey, ABI]:
-    """Return prepared instance of CachedStorage with local files provider."""
+    """
+    Return prepared instance of CachedStorage with local files provider.
+
+    :param interfaces_directory: str, path to directory with interfaces.
+    :return: CachedStorage[ABIKey, ABI]
+    """
     return CachedStorage(ABIProviderLocalDirectory(interfaces_directory))
 
 
@@ -186,7 +197,14 @@ def get_cached_combined(
         api_key: str, net: str,
         interfaces_directory: str
 ) -> CachedStorage[Tuple[ABIKey, ABIKey], ABI]:
-    """Return prepared instance of CachedStorage with combined provider."""
+    """
+    Return prepared instance of CachedStorage with combined provider.
+
+    :param api_key: str, Etherscan API token.
+    :param net: str, the name of target network.
+    :param interfaces_directory: str, path to directory with interfaces.
+    :return: CachedStorage[ABIKey, ABI]
+    """
     return CachedStorage(ABIProviderCombined(
         api_key, net, interfaces_directory
     ))

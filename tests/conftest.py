9cf60ca3d09b69abc460b84d0c8c985772ce0abb
Dmitri Ivakhnenko <dmit.ivh@gmail.com>
2021-09-20 13:31:26 +0300

    Tests: update part for decoding

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,43 +1,5 @@
 """Settings for tests."""
-from functools import partial
-from typing import List
-
 import pytest
-
-from examples import ABIEtherscan
-
-
-def _parametrize(
-        metafunc, fixture_name: str, cases: List
-) -> None:
-    if fixture_name in metafunc.fixturenames:
-        if hasattr(_parametrize, fixture_name):
-            cases, names = getattr(
-                _parametrize, fixture_name
-            )
-
-        else:
-            names = [
-                f'case_{ind}'
-                for ind in range(len(cases))
-            ]
-
-            setattr(
-                _parametrize, fixture_name, (cases, names)
-            )
-
-        metafunc.parametrize(
-            fixture_name, cases, ids=names
-        )
-
-
-def pytest_generate_tests(metafunc):
-    """Parametrize tests."""
-    parametrize = partial(_parametrize, metafunc)
-    for fixture_name, cases in [
-        ('abi_positive_example', ABIEtherscan.positive_examples)
-    ]:
-        parametrize(fixture_name, cases)
 
 
 def pytest_addoption(parser):
--- tests/test_abi.py
+++ tests/test_abi.py
@@ -1,13 +1,43 @@
 """Tests of getting ABI from different sources."""
 import os
+from collections import namedtuple
+
 import pytest
 
-from evmscript_parser.core.decode import decode_function_call
 from evmscript_parser.core.ABI import get_cached_combined
 from evmscript_parser.core.ABI.storage import CachedStorage, ABIKey
+from evmscript_parser.core.decode import decode_function_call
 
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 INTERFACES = os.path.join(CUR_DIR, 'interfaces')
+
+FunctionCall = namedtuple(
+    'FunctionCall',
+    field_names=['address', 'signature', 'name', 'call_data'],
+)
+
+positive_examples = (
+    # Tether
+    FunctionCall(
+        address='0xdac17f958d2ee523a2206206994597c13d831ec7',
+        signature='0x18160ddd',
+        name='totalSupply',
+        call_data=''
+    ),
+    # Lido
+    FunctionCall(
+        address='0xae7ab96520de3a18e5e111b5eaab095312d7fe84',
+        signature='0x18160ddd',
+        name='totalSupply',
+        call_data=''
+    )
+)
+
+
+@pytest.fixture(scope='module', params=positive_examples)
+def positive_example(request):
+    """Get positive test case for call decoding."""
+    return request.param
 
 
 @pytest.fixture(scope='module')
@@ -18,11 +48,12 @@ def abi_storage(api_key: str) -> CachedStorage:
     )
 
 
-def test_etherscan_api(abi_storage, abi_positive_example):
+def test_etherscan_api(abi_storage, positive_example: FunctionCall):
     """Run tests for getting ABI from Etherscan API."""
-    interface_name, contract, sign, name = abi_positive_example
     assert decode_function_call(
-        contract, sign,
-        '', abi_storage
-    ).function_name == name
-    assert ABIKey(contract, sign) in abi_storage
+        positive_example.address, positive_example.signature,
+        positive_example.call_data, abi_storage
+    ).function_name == positive_example.name
+    assert ABIKey(
+        positive_example.address, positive_example.signature
+    ) in abi_storage
